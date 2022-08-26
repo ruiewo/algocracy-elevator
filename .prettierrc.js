f47876a8de7deecf41dea35fffd598e0c1a200f7
ruiewo <109674512+ruiewo@users.noreply.github.com>
2022-08-26 10:51:17 +0900

    add prettier.

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    singleQuote: true,
+    trailingComma: 'es5',
+    printWidth: 150,
+    tabWidth: 4,
+    quoteProps: 'as-needed',
+    semi: true,
+    arrowParens: 'avoid',
+};
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -4,7 +4,7 @@ import Editor from 'react-simple-code-editor';
 import { highlight, languages } from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-javascript';
-import 'prismjs/themes/prism.css'; //Example style, you can use another
+import 'prismjs/themes/prism.css'; 
 
 export default function CodeEditor() {
   const [code, setCode] = React.useState(
