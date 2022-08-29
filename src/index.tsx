fa17428b584dd3aba1fa4ebd413adc2a433443b9
ruiewo <109674512+ruiewo@users.noreply.github.com>
2022-08-29 17:35:54 +0900

    change logo.

--- src/index.tsx
+++ src/index.tsx
@@ -3,21 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import SignIn from './signin';
-import CodeEditor from './components/Editor';
+import CodeEditor from './components/CodeEditor';
 import TitleBar from './components/TitleBar';
 import Game from './components/Game';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <TitleBar/>
-    {/* <App /> */}
-    <Game />
-    <CodeEditor />
-    <SignIn />
-  </React.StrictMode>
+    <React.StrictMode>
+        <TitleBar />
+        {/* <App /> */}
+        <Game />
+        <CodeEditor />
+        <SignIn />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
